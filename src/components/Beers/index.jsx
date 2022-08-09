409c977be48dd8c652a3649659c50aa06c691785
james etchberger <etchbergerjames@gmail.com>
2022-08-09 11:42:12 -0400

    styled beer cards

--- src/components/Beers/index.jsx
+++ src/components/Beers/index.jsx
@@ -1,32 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import BeerCard from './BeerCard'
 
-function Beers () {
-    const [beerList, setBeerList] = useState()
-    const [beerType, setBeerType] = useState('ale')
-    useEffect(() => {
-        fetch(`https://api.sampleapis.com/beers/${beerType}`) //ale, stouts
-        .then(response => response.json())
-        .then(beers => setBeerList(beers))
-        .catch(alert)
-    }, [beerType])
-    if(!beerList) {
-        return <h2>Loading...</h2>
-    }
-    return (
-        <>
-        <button onClick={() => setBeerType('ale')}>Ale</button>
-        <button onClick={() => setBeerType('stouts')}>Stouts</button>
-        {/* <ul> */}
-            {beerList.map(beer => (
-                <ul>
-                <li key={beer.id}><b>{beer.name}</b></li>
-                <li key={beer.id}>{beer.price}</li>
-                </ul>
-            ))}
-        {/* </ul> */}
-        </>
-    )
-
+function Beers() {
+  const [beerList, setBeerList] = useState()
+  const [beerType, setBeerType] = useState('ale')
+  useEffect(() => {
+    fetch(`https://api.sampleapis.com/beers/${beerType}`) //ale, stouts
+      .then((response) => response.json())
+      .then((beers) => setBeerList(beers))
+      .catch(alert)
+  }, [beerType])
+  if (!beerList) {
+    return <h2>Loading...</h2>
+  }
+  return (
+    <>
+      <button onClick={() => setBeerType('ale')}>Ale</button>
+      <button onClick={() => setBeerType('stouts')}>Stouts</button>
+      <section id='beerList'>
+      {beerList.map(beer => (
+          <BeerCard key={beer.id} beer={beer} />
+      ))}
+      </section>
+    </>
+  )
 }
 
 export default Beers
